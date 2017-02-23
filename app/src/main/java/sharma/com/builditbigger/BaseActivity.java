da8606fc696a6b6841de9760eee53ab1e218ffc7
Ankit Sharma <ankit@intrepid.io>
2017-02-23 13:22:17 -0500

    Added async task

--- app/src/main/java/sharma/com/builditbigger/BaseActivity.java
+++ app/src/main/java/sharma/com/builditbigger/BaseActivity.java
@@ -1,12 +1,16 @@
 package sharma.com.builditbigger;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-import com.example.JavaJokes;
 import com.example.ankitsharma.myapplication.backend.myApi.MyApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
@@ -32,23 +36,52 @@ public class BaseActivity extends AppCompatActivity {
 
     @OnClick(R.id.joke_button)
     public void jokeButtonClicked() {
-        new EndpointsAsyncTask().execute(this);
+        if (networkConnected()) {
+            EndpointsAsyncTask asyncTask = new EndpointsAsyncTask(this);
+            asyncTask.execute();
+        } else {
+            Toast.makeText(this, getString(R.string.network_not_available), Toast.LENGTH_SHORT)
+                    .show();
+        }
+
+    }
+
+    public boolean networkConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        return (activeNetwork != null) && (activeNetwork.isConnected());
     }
 }
 
-class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
+class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
     private static MyApi myApiService = null;
-    private Context context;
+    private Context activity;
+
+    private ProgressDialog dialog;
+
+    public EndpointsAsyncTask(Activity activity) {
+        super();
+        this.activity = activity;
+        dialog = new ProgressDialog(activity);
+    }
 
     @Override
-    protected String doInBackground(Context... params) {
+    protected void onPreExecute() {
+        dialog.show();
+        dialog.setMessage(activity.getString(R.string.progress_bar_title));
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
         if(myApiService == null) {  // Only do this once
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
                     .setRootUrl("https://builditbigger-158813.appspot.com/_ah/api/");
 
             myApiService = builder.build();
         }
-        context = params[0];
+
         try {
             return myApiService.getJoke().execute().getData();
         } catch (IOException e) {
@@ -58,9 +91,11 @@ class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
 
     @Override
     protected void onPostExecute(String result) {
-        JavaJokes javaJokes = new JavaJokes();
-        Intent intent = new Intent(context, AndroidJokes.class);
-        intent.putExtra("JOKE", javaJokes.getJoke());
-        context.startActivity(intent);
+        if (dialog.isShowing()) {
+            dialog.dismiss();
+        }
+        Intent intent = new Intent(activity, AndroidJokes.class);
+        intent.putExtra("JOKE", result);
+        activity.startActivity(intent);
     }
 }
--- javajokelibrary/src/main/java/com/example/JavaJokes.java
+++ javajokelibrary/src/main/java/com/example/JavaJokes.java
@@ -1,14 +1,22 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JavaJokes {
 
-    private String joke = "Your python drank my java!";
+    private static final List<String> jokes = new ArrayList<String>() {{
+        add("Your python drank my Java");
+        add("There are only 10 types of people in the world: those that understand binary and those that don’t.");
+        add("Artificial intelligence usually beats real stupidity.");
+        add("CAPS LOCK – Preventing Login Since 1980.");
+        add("The truth is out there. Anybody got the URL?");
+        add("Unix is user friendly. It’s just selective about who its friends are.");
+        add("Failure is not an option. It comes bundled with your Microsoft product.");
+    }};
 
     public String getJoke() {
-        return joke;
-    }
-
-    public void setJoke(String joke) {
-        this.joke = joke;
+        int size = jokes.size();
+        return jokes.get((int) (Math.random() * (size-1)));
     }
 }
